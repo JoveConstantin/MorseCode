48ebc783123aae1d2a7d1443ca8713d6f2f0b829
Konstanin Efremov <efremov.kost@ya.ru>
2025-04-26 22:58:08 +0300

    first commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	server "github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
 func main() {
+	logger := log.New(os.Stdout, "MyServer:", log.LstdFlags)
+	server := server.MyRouter(logger)
+	if err := server.Server.ListenAndServe(); err != nil {
+		logger.Fatal("Server failed: ", err)
+	}
 
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,66 @@
 package handlers
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	service "github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func MainHandle(w http.ResponseWriter, req *http.Request) {
+	filePath := filepath.Join("..", "index.html")
+	file, err := os.Open(filePath)
+	if err != nil {
+		http.Error(w, "File not found", http.StatusNotFound)
+		return
+	}
+	defer file.Close()
+	_, err = io.Copy(w, file)
+	if err != nil {
+		http.Error(w, "Error reading file", http.StatusInternalServerError)
+	}
+}
+
+func UploadHandle(w http.ResponseWriter, req *http.Request) {
+
+	err := req.ParseMultipartForm(10 << 20)
+	if err != nil {
+		http.Error(w, "Error parsing form", http.StatusInternalServerError)
+		return
+	}
+	file, header, err := req.FormFile("myFile")
+	if err != nil {
+		http.Error(w, "Error retrieving file", http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+
+	fileBytes, err := io.ReadAll(file)
+	if err != nil {
+		http.Error(w, "Error reading file", http.StatusInternalServerError)
+		return
+	}
+	fileStrings := string(fileBytes)
+	convertData, err := service.MorseOrTextConverter(fileStrings)
+	outputFilename := time.Now().UTC().Format("20060102_150405") + filepath.Ext(header.Filename)
+	outputFile, err := os.Create(outputFilename)
+	if err != nil {
+		http.Error(w, "Failed to create file", http.StatusInternalServerError)
+		return
+	}
+	defer outputFile.Close()
+
+	if _, err := outputFile.WriteString(convertData); err != nil {
+		http.Error(w, "Failed to write result to file", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Converted data:\n%s", convertData)
+
+}
